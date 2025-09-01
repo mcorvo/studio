5041e67eb4f5a348ba8f95f63a589db450d54f0b
Marco Corvo <corvo@cnaf.infn.it>
2025-09-01 17:31:06 +0200

    Fix prisma schema and data type

--- src/app/api/rda/route.ts
+++ src/app/api/rda/route.ts
@@ -30,11 +30,7 @@ export async function POST(request: Request) {
 
     const rdasToCreate: Prisma.RdaCreateManyInput[] = body.map(item => {
       const anno = parseInt(String(item.anno), 10);
-<<<<<<< Updated upstream
-      const licenseId = licenseMap.get(item.prodotto);
-=======
       const licenseId = licenseMap.get(item.prodotto)!;
->>>>>>> Stashed changes
       
       return {
         rda: String(item.rda ?? ''),
